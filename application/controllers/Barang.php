f11b54315629860c230ff5279da31bf441b8237e
234iwang <66737765+234iwang@users.noreply.github.com>
2024-02-13 17:24:43 +0700

    first commit

---
+++ application/controllers/Barang.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Barang extends CI_Controller
+{
+
+    public function index()
+    {
+        $data['title'] = 'Barang';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('inventory/barang');
+        $this->load->view('templates/auth_footer');
+    }
+}
---
+++ application/views/inventory/barang.php
@@ -0,0 +1,26 @@
+   <div class="content-wrapper">
+       <!-- START PAGE CONTENT-->
+       <div class="page-heading">
+           <h1 class="page-title">DataTables</h1>
+           <ol class="breadcrumb">
+               <li class="breadcrumb-item">
+                   <a href="index.html"><i class="la la-home font-20"></i></a>
+               </li>
+               <li class="breadcrumb-item">DataTables</li>
+           </ol>
+       </div>
+       <div class="page-content fade-in-up">
+           <div class="ibox">
+               <div class="ibox-head">
+                   <div class="ibox-title">Data Table</div>
+               </div>
+               <div class="ibox-body">
+                   <table class="table table-striped table-bordered table-hover" id="example-table" cellspacing="0" width="100%">
+                       <thead>
+                           <tr>
+                               <th>id</th>
+                               <th>nama_barang</th>
+                               <th>stok</th>
+                               <th>Satuan</th>
+                               <th>Jenis_id</th>
+                           </tr>
